d7f3df6fe55d9a729fd945c7239c7723bc7671b2
aleksandr.developer.web <aleksandr.developer.web@gmail.com>
2023-08-15 14:01:22 +0200

    test reset 2

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -20,6 +20,9 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          const Center(
+            child: Text('This new text'),
+          ),
          const Center(
             child: Text('HomeScreen new variable'),
           ),
